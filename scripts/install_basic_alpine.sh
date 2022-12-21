3974e394cd521c76d0bac12678c245d2b35163b6
Alexander Piskun <bigcat88@icloud.com>
2022-12-21 12:26:31 +0300

    dev: v0.0.5

--- scripts/install_basic_alpine.sh
+++ scripts/install_basic_alpine.sh
@@ -10,14 +10,13 @@ apk add --no-cache \
   autoconf \
   automake \
   cmake \
-  nasm \
   python3-dev \
   py3-pip && \
   echo "**** Installing Patchelf ****" && \
   git clone -b 0.17.0 https://github.com/NixOS/patchelf.git && \
   cd patchelf && \
   ./bootstrap.sh && ./configure && make && make check && make install && \
-  cd .. && rm -rf ./patchelf \
+  cd .. && rm -rf ./patchelf && \
   echo "**** Installing ccache ****" && \
   git clone -b v4.7.4 https://github.com/ccache/ccache.git && \
   cd ccache && \
